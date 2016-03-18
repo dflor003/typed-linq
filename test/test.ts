1635e2f04e9e5314cdb9c6dfc207cdf34a88308a
DanilF <dflor003@gmail.com>
2016-03-18 01:21:57 -0400

    DanilF - Fixed some tests. Fixed some defs.

--- test/test.ts
+++ test/test.ts
@@ -1,39 +1,45 @@
 /// <reference path="./main.d.ts"/>
 
 import * as Enumerable from 'linq';
-import {IGrouping} from 'linq';
 
 describe("Linq.js tests", function () {
-  it("Projection and Filtering Methods", function () {
-    expect(Enumerable.from([1,2,3,4]).ToString(",")).toBe("1,2,3,4");
-    expect(Enumerable.range(1, 4).Where((item: number) => item > 2).ToString(",")).toBe("3,4");
-    expect(Enumerable.range(1, 4).Where("(item) => item > 2").ToString(",")).toBe("3,4");
-    expect(Enumerable.range(1, 4).Where("$>2").ToString(",")).toBe("3,4");
-    expect(Enumerable.range(1, 4).Select((item: number,index:number) => item+index).ToString(",")).toBe("1,3,5,7");
-    expect(Enumerable.range(1, 4).Zip(Enumerable.range(1, 10),  (a: number,b:number) => a-b).ToString(",")).toBe("0,0,0,0");
+    it("Projection and Filtering Methods", function () {
+        let x = Enumerable.from([1, 2, 3, 4]).toJoinedString(",");
+        expect(x).toBe("1,2,3,4");
+        expect(Enumerable.range(1, 4).where(item => item > 2).toJoinedString(",")).toBe("3,4");
+        expect(Enumerable.range(1, 4).where("(item) => item > 2").toJoinedString(",")).toBe("3,4");
+        expect(Enumerable.range(1, 4).where("$>2").toJoinedString(",")).toBe("3,4");
+        expect(Enumerable.range(1, 4).select((item, index) => item + index).toJoinedString(",")).toBe("1,3,5,7");
+        expect(Enumerable.range(1, 4).zip(Enumerable.range(1, 10), (a: number, b: number) => a - b).toJoinedString(",")).toBe("0,0,0,0");
 
-  });
-  it("Join Methods", function () {
-    expect(Enumerable.range(1, 4).Join(Enumerable.from(["a", "aaa"]), (l) => l, (r: string) => r.length, (l, r) =>l + ":" + r).ToString(",")).toBe("1:a,3:aaa");
-  });
-  it("Set Methods", function () {
-    expect(Enumerable.range(2, 4, 2).All((item: number) => item % 2 == 0)).toBe(true);
-    expect(Enumerable.range(1, 4).Intersect(Enumerable.range(3, 4)).ToString(",")).toBe("3,4");
-  });
-  it("Ordering Methods", function () {
-    expect(Enumerable.from(
-      [
-        { name: "marcin", age:15},
-        { name: "albert", age:51},
-        { name: "marcin", age:30},
-      ]).OrderBy((p) => p.name).ThenByDescending((p) => p.age).Select((p) => p.name+p.age).ToString(",")).toBe("albert51,marcin30,marcin15");
-  });
-  it("Grouping Methods", function () {
-    expect(Enumerable.from(["a","aa","aaa","a","a","aaa"])
-      .GroupBy((item:string) => item.length)
-      .Select((g: IGrouping<string>) => { return { key: g.Key(), count: g.Count() }; })
-      .OrderBy(g => g.key)
-      .Select(g => g.key+":"+g.count)
-      .ToString(",")).toBe("1:3,2:1,3:2");
-  });
+    });
+    it("Join Methods", function () {
+        let other = Enumerable.from(["a", "aaa"]);
+        let actual = Enumerable
+            .range(1, 4)
+            .join(other, num => num, str => str.length, (num, len) => num + ":" + len).toJoinedString(",");
+        expect(actual).toBe("1:a,3:aaa");
+    });
+    it("Set Methods", function () {
+        expect(Enumerable.range(2, 4, 2).all((item: number) => item % 2 == 0)).toBe(true);
+        expect(Enumerable.range(1, 4).intersect(Enumerable.range(3, 4)).toJoinedString(",")).toBe("3,4");
+    });
+    it("Ordering Methods", function () {
+        expect(Enumerable.from(
+            [
+                { name: "marcin", age: 15 },
+                { name: "albert", age: 51 },
+                { name: "marcin", age: 30 },
+            ]).orderBy((p) => p.name).thenByDescending((p) => p.age).select((p) => p.name + p.age).toJoinedString(",")).toBe("albert51,marcin30,marcin15");
+    });
+    it("Grouping Methods", function () {
+        expect(Enumerable.from(["a", "aa", "aaa", "a", "a", "aaa"])
+            .groupBy((item: string) => item.length)
+            .select(g => {
+                return { key: g.key(), count: g.count() };
+            })
+            .orderBy(g => g.key)
+            .select(g => g.key + ":" + g.count)
+            .toJoinedString(",")).toBe("1:3,2:1,3:2");
+    });
 });
